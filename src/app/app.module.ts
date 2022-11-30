37a1ed30644adf939438f96b99b10e443856c55a
Rajada29 <rajnaiadam29@gmail.com>
2022-11-30 19:41:19 +0100

    Angular 2
    xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './user/user.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    UserComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
